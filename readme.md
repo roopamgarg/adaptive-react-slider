14d5fcc338e22d2bb24b16b8ba673a9d9db2735d
roopamgarg <roopamg777@gmail.com>
2020-11-24 20:22:44 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## React Slider Component 
+
+### Dependencies Required
+node.js[]
+
+### Steps to run
+
+```
+npm i
+npm start
+```
---
+++ src/app.js
@@ -0,0 +1,37 @@
+import React, {useState,useRef,useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import Slider from "./Components/Slider/Slider.jsx";
+import Slide from "./Components/Slider/Slide.jsx";
+
+import "./Styles/index.scss";
+
+const bg = [
+    "#f15946",
+    "#571ec1",
+    "#636567",
+    "#571eb1",
+    "#f7f567",
+
+]
+const noOfSlides = bg.length;
+
+const slidesPerPage = 2;
+const App = () => {
+    
+    const createSlides = () => {
+        return bg.map((cur,index) => (
+           <Slide key={index} style={{backgroundColor:cur, height:"100vh"}} slidesPerScreen={slidesPerPage} className="slide" >
+                {cur}
+           </Slide>
+        ))
+    }
+    return (
+        <div className="container">
+            <Slider noOfSlides={noOfSlides} slidesPerScreen={slidesPerPage} startWith={1}>
+                {createSlides()}
+            </Slider>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
